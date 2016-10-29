0ebe24338af2772632bc67a30ed7ccca01f7e745
pablodiegoss <pablodiegoss@hotmail.com>
2016-10-28 23:03:31 -0200

    fix Nome Completo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    nome_completo = (params[:first_name])#.to_s +" " + (params[:last_name]).to_s
+    primeiro_nome = params[:first_name]
+    ultimo_nome = params[:last_name]
+    nome_completo = primeiro_nome +" "+ultimo_nome;
     usuario = params[:email]
     senha = params[:password_digest]
-    codigo_verificacao = "Lorem ipsum dolor"
+    codigo_verificacao = SecureRandom.urlsafe_base64(nil, false);
 
     user = User.new(nome_completo: nome_completo, email: usuario,usuario: usuario, senha: senha, codigo_verificacao: codigo_verificacao)
     if user.save
