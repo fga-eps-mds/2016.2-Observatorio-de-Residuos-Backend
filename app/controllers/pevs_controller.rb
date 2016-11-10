4e7192c3f227598b559d73d927c795386e131109
Jefersonalves <jefersonalvesf2009@gmail.com>
2016-11-10 21:32:13 -0200

    Add attibutes for evaluate pevs

--- app/controllers/pevs_controller.rb
+++ app/controllers/pevs_controller.rb
@@ -19,6 +19,17 @@ class PevsController < ApplicationController
     render json: pev;
   end
 
+###################################################################
+  def increment
+    #pev = Pev.find_by_id_pev(params[:])
+    pev = Pev.find_by_latitude_and_longitude(params[:pev][:latitude], params[:pev][:longitude])
+    pev.total_confirmacoes_funcionando = params[:likes]
+    pev.total_confirmacoes_fechou = params[:dislikes]
+    pev.save
+    render json: pev;
+  end
+####################################################################
+
   def create
     titulo_pev = params[:name]
     descricao_pev = params[:description]
@@ -31,13 +42,21 @@ class PevsController < ApplicationController
     metal = params[:metal]
     plastic = params[:plastic]
     glass = params[:glass]
-    id_usuario = User.find_by_email(params[:author_email]).id_usuario;
+###################################################################
+    total_confirmacoes_funcionando = params[:likes]
+    total_confirmacoes_fechou = params[:dislikes]
+###################################################################
 
+    id_usuario = User.find_by_email(params[:author_email]).id_usuario;
     pev = Pev.new(titulo_pev: titulo_pev, descricao_pev: descricao_pev,
                   id_tipo_pev: id_tipo_pev, latitude: latitude,
                   longitude: longitude, estado: estado, cidade: cidade,
                   id_usuario: id_usuario,paper: paper, metal: metal,
-                  plastic: plastic, glass: glass)
+                  plastic: plastic, glass: glass,
+###################################################################
+                  total_confirmacoes_funcionando: total_confirmacoes_funcionando,
+                  total_confirmacoes_fechou: total_confirmacoes_fechou)
+###################################################################
     if pev.save
         render json: pev
     else
@@ -48,6 +67,8 @@ class PevsController < ApplicationController
 
   private
   def pev_params
-    params.require(:pev).permit(:author_email, :name, :plastic, :paper, :metal, :glass, :latitude, :longitude, :description)
+###################################################################
+    params.require(:pev).permit(:author_email, :name, :plastic, :paper, :metal, :glass, :latitude, :longitude, :description, :likes, :dislikes) 
+###################################################################
   end
 end
