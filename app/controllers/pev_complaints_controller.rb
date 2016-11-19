07b72a565cab602436f22b4ef430667493ea0dc9
gustavo2795 <gustavo.unb27@gmail.com>
2016-11-19 15:20:42 -0200

    Create pev complaints controller

---
+++ app/controllers/pev_complaints_controller.rb
@@ -0,0 +1,19 @@
+class PevComplaintsController < ApplicationController
+  def index
+    render json: PevComplaint.all
+  end
+
+  def create
+    title = params[:title]
+    description = params[:description]
+    author = params[:author]
+    id_pev = params[:id_pev]
+    pevcomplaint = PevComplaint.new(title: title, description: description, author: author, id_pev: id_pev)
+    if pevcomplaint.save
+        render json: pevcomplaint
+    else
+        render json: { error: 'Incorrect credentials' }, status: 401
+        puts pevcomplaint.errors.messages
+    end
+  end
+end
